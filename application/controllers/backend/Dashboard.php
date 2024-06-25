cb03e3c8ce2127d3b4c6ab17711f2c533adaf7dd
Fauzan Bintang <fauzanbintang121@gmail.com>
2024-06-25 22:46:16 +0700

    first commit

---
+++ application/controllers/backend/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends MY_Controller
+{
+
+	public function index()
+	{
+		view('backend/Dashboard/index');
+	}
+
+	public function table()
+	{
+		$daftar_user = \Orm\User::all();
+		view('backend/Dashboard/table', ['daftar_user' => $daftar_user]);
+	}
+
+	public function hapus($id)
+	{
+		$user = \Orm\User::find($id);
+		$user->delete();
+		redirect('backend/Dashboard/table');
+	}
+}
